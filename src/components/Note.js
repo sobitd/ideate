4e4f5983cd6a4035d00fd501ef50e3ebced6fe2d
Daniel Sobit <sobitdaniel@gmail.com>
2022-09-12 13:13:43 +0300

    Initialize project using Create React App

---
+++ src/components/Note.js
@@ -0,0 +1,10 @@
+const Note = () => {
+  return <div className="note">
+    <span> Note me </span>
+    <div className="note-footer">
+        <small>date</small>
+    </div>
+  </div>;
+};
+
+export default Note;
